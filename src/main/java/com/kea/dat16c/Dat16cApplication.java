b2ee8772cbad94b82bc00fe62d539774b093a4e3
joneikholm <joneikholm@gmail.com>
2017-10-04 22:59:18 +0200

    first

---
+++ src/main/java/com/kea/dat16c/Dat16cApplication.java
@@ -0,0 +1,12 @@
+package com.kea.dat16c;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dat16cApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dat16cApplication.class, args);
+	}
+}
---
+++ src/main/java/com/kea/dat16c/HomeController.java
@@ -0,0 +1,23 @@
+package com.kea.dat16c;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class HomeController {
+
+
+    @RequestMapping({"", "/", "index"})
+    public String index(Model model){
+        model.addAttribute("user", "Dat16c site");
+        return "index";
+    }
+
+
+}
