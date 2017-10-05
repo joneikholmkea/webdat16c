c050f14ecd94178b7cc0ea43ef2a5b41dbfa39a4
Simon <simo913z@stud.kea.dk>
2017-10-05 11:39:42 +0200

    update

---
+++ src/main/java/com/kea/dat16c/ProjectLinks.java
@@ -0,0 +1,44 @@
+package com.kea.dat16c;
+import java.util.ArrayList;
+
+public class ProjectLinks
+{
+
+
+    private String link;
+    private String projectName;
+    private String name;
+    private String summary;
+
+
+    @Override
+    public String toString() {
+        return "Link" + link + projectName + name + summary;
+    }
+
+    public ProjectLinks()
+    {
+
+    }
+
+    public ProjectLinks(String link, String projectText) {
+        this.link = link;
+        this.projectText = projectText;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getProjectText() {
+        return projectText;
+    }
+
+    public void setProjectText(String projectText) {
+        this.projectText = projectText;
+    }
+}
