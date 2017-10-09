e04283a0d77bdfd8cccdfaf47423ab0db9285d3a
Magnus Thygesen <thygesen92@hotmail.dk>
2017-10-09 10:08:20 +0200

    Made fun site, missing content

--- src/main/java/com/kea/dat16c/FunController.java
+++ src/main/java/com/kea/dat16c/FunController.java
@@ -2,16 +2,30 @@ package com.kea.dat16c;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class FunController
 {
-
+    List<FunLink> links = new ArrayList<>();
 
     @RequestMapping({"fun"})
     public String fun(Model model){
         model.addAttribute("user", "Dat16c site");
+        return "fun";
+    }
+
+
+    @RequestMapping(value = {"fun"}, method = RequestMethod.POST)
+    public String handleSubmit(@ModelAttribute FunLink link, Model model)
+    {
+        links.add(link);
+        model.addAttribute("links", links);
         return "fun";
     }
 
---
+++ src/main/java/com/kea/dat16c/FunLink.java
@@ -0,0 +1,36 @@
+package com.kea.dat16c;
+
+import java.net.URL;
+
+/**
+ * Created by thyge on 05-10-2017.
+ */
+public class FunLink
+{
+    String link;
+
+    public FunLink()
+    {
+
+    }
+
+    public FunLink(String link)
+    {
+        this.link = link;
+    }
+
+    public String getLink()
+    {
+        return link;
+    }
+
+    public void setLink(String link)
+    {
+        this.link = link;
+    }
+
+    public String toString()
+    {
+        return link;
+    }
+}
