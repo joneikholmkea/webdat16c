61e9f85b3eb4c83991a1b02b106eeac0681b1612
Rasmus Thrane <snubi68@gmail.com>
2017-10-10 13:50:43 +0200

    edited

--- src/main/java/com/kea/dat16c/FunController.java
+++ src/main/java/com/kea/dat16c/FunController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,8 +16,13 @@ public class FunController
     List<FunLink> links = new ArrayList<>();
 
     @RequestMapping({"fun"})
-    public String fun(Model model){
-        model.addAttribute("user", "Dat16c site");
+    public String fun(Model model, HttpSession session){
+        User user = (User)session.getAttribute("isLoggedIn");
+        if(user != null){
+            model.addAttribute("user", user);
+        }else{
+            model.addAttribute("user", "Dat16c site");
+        }
         return "fun";
     }
 
