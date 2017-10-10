eafdebb253ccae0d6f97832d6f92f621b3066be0
Simon <simo913z@stud.kea.dk>
2017-10-10 13:30:16 +0200

    login init

--- src/main/java/com/kea/dat16c/ProjectController.java
+++ src/main/java/com/kea/dat16c/ProjectController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,7 +16,12 @@ public class ProjectController
     List<ProjectLinks> links = new ArrayList<>();
 
         @RequestMapping({"", "/", "projects"})
-        public String index(Model model){
+        public String index(Model model, HttpSession session){
+            User user = (User)session.getAttribute("isLoggedIn");
+            if(user != null){
+                model.addAttribute("user", user.getUsername());
+                return "index";
+            }
             model.addAttribute("user", "Dat16c site");
             return "projects";
         }
